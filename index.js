2cf18119e6e1cc3e2b32971d3bbe6a0bd7ad62f6
injashkin <injashkin@gmail.com>
2019-09-30 23:50:40 +0300

    Layout and initial design

--- index.js
+++ index.js
@@ -1,66 +1,94 @@
 function App() {
     return React.createElement(
         'div',
-        null,
+        { className: 'clock' },
         React.createElement(
             'div',
-            { id: 'break-label' },
-            'Break Length'
-        ),
-        React.createElement(
-            'div',
-            { id: 'session-label' },
-            'Session Length'
-        ),
-        React.createElement(
-            'button',
-            { id: 'break-decrement' },
-            'break-decrement'
-        ),
-        React.createElement(
-            'button',
-            { id: 'session-decrement' },
-            'session-decrement'
-        ),
-        React.createElement(
-            'button',
-            { id: 'break-increment' },
-            'break-increment'
-        ),
-        React.createElement(
-            'button',
-            { id: 'session-increment' },
-            'session-increment'
+            { className: 'timer-output' },
+            React.createElement(
+                'div',
+                { id: 'timer-label' },
+                'Session||Break'
+            ),
+            React.createElement(
+                'div',
+                { id: 'time-left' },
+                '25:00'
+            )
         ),
         React.createElement(
             'div',
-            { id: 'break-length' },
-            '5'
+            { className: 'btn-control' },
+            React.createElement(
+                'button',
+                { id: 'start_stop' },
+                'Start||Stop'
+            ),
+            React.createElement(
+                'button',
+                { id: 'reset' },
+                'Reset'
+            )
         ),
         React.createElement(
             'div',
-            { id: 'session-length' },
-            '25'
-        ),
-        React.createElement(
-            'div',
-            { id: 'timer-label' },
-            'session'
-        ),
-        React.createElement(
-            'div',
-            { id: 'time-left' },
-            '25:00'
-        ),
-        React.createElement(
-            'button',
-            { id: 'start_stop' },
-            'start_stop'
-        ),
-        React.createElement(
-            'button',
-            { id: 'reset' },
-            'reset'
+            { className: 'setting' },
+            React.createElement(
+                'div',
+                { className: 'break-wrap' },
+                React.createElement(
+                    'div',
+                    { id: 'break-label' },
+                    'Break Length'
+                ),
+                React.createElement(
+                    'div',
+                    { className: 'break-setting' },
+                    React.createElement(
+                        'button',
+                        { id: 'break-decrement' },
+                        '-'
+                    ),
+                    React.createElement(
+                        'div',
+                        { id: 'break-length' },
+                        '5'
+                    ),
+                    React.createElement(
+                        'button',
+                        { id: 'break-increment' },
+                        '+'
+                    )
+                )
+            ),
+            React.createElement(
+                'div',
+                { className: 'session-wrap' },
+                React.createElement(
+                    'div',
+                    { id: 'session-label' },
+                    'Session Length'
+                ),
+                React.createElement(
+                    'div',
+                    { className: 'session-setting' },
+                    React.createElement(
+                        'button',
+                        { id: 'session-decrement' },
+                        '-'
+                    ),
+                    React.createElement(
+                        'div',
+                        { id: 'session-length' },
+                        '25'
+                    ),
+                    React.createElement(
+                        'button',
+                        { id: 'session-increment' },
+                        '+'
+                    )
+                )
+            )
         )
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
 function App() {
     return (
-        <div>
-            <div id='break-label'>Break Length</div>
-            <div id='session-label'>Session Length</div>
-            <button id='break-decrement'>break-decrement</button>
-            <button id='session-decrement'>session-decrement</button>
-            <button id='break-increment'>break-increment</button>
-            <button id='session-increment'>session-increment</button>
-            <div id='break-length'>5</div>
-            <div id='session-length'>25</div>
-            <div id='timer-label'>session</div>
-            <div id='time-left'>25:00</div>
-            <button id='start_stop'>start_stop</button>
-            <button id='reset'>reset</button>
+        <div className='clock'>
+            <div className='timer-output'> 
+                <div id='timer-label'>Session||Break</div>
+                <div id='time-left'>25:00</div>
+            </div>
+            <div className='btn-control'>
+                <button id='start_stop'>Start||Stop</button>
+                <button id='reset'>Reset</button>
+            </div>
+            
+            <div className='setting'>
+                <div className='break-wrap'>
+                    <div id='break-label'>Break Length</div>
+                    <div className='break-setting'>
+                        <button id='break-decrement'>-</button>
+                        <div id='break-length'>5</div>
+                        <button id='break-increment'>+</button>
+                    </div>
+                </div>
+                <div className='session-wrap'>
+                    <div id='session-label'>Session Length</div>
+                    <div className='session-setting'>
+                        <button id='session-decrement'>-</button>
+                        <div id='session-length'>25</div>
+                        <button id='session-increment'>+</button>
+                    </div>
+                </div>
+            </div>            
         </div>
     )
 }
